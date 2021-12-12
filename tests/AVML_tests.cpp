6ca00967e872549a159c8455052a61b2693202e8
Avereniect <Avereniect@gmail.com>
2021-12-11 21:08:25 -0800

    Minor updates to main test file

--- tests/AVML_tests.cpp
+++ tests/AVML_tests.cpp
@@ -1,13 +1,8 @@
 #include <gtest/gtest.h>
 
-#include "Unit_vector_tests.hpp"
-#include "Vector_tests.hpp"
-
 #include "matrix/Mat2x2f_tests.hpp"
 #include "matrix/Mat3x3f_tests.hpp"
 #include "matrix/Mat4x4f_tests.hpp"
-
-#include "affine/Affine3Df_tests.hpp"
 
 int main(int argc, char* argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
